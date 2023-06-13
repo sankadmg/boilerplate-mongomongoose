1da2012b7bf566260b0a208b762757f96864f762
Dileepa Madusanka <sanka.dmg@gmail.com>
2023-06-13 19:26:43 +0530

    modified createAndSavePerson function

--- myApp.js
+++ myApp.js
@@ -18,7 +18,16 @@ const personSchema = new Schema({
 const Person = mongoose.model("Person", personSchema);
 
 const createAndSavePerson = (done) => {
-  done(null /*, data*/);
+  var janeFonda = new Person({
+    name: "Jane Fonda",
+    age: 84,
+    favoriteFoods: ["eggs", "fish", "fresh fruit"],
+  });
+
+  janeFonda.save(function (err, data) {
+    if (err) return console.error(err);
+    done(null, data);
+  });
 };
 
 const createManyPeople = (arrayOfPeople, done) => {
